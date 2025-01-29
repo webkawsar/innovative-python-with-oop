81a5e13bd5e8918e148769e14d25639a7af7ed97
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-01-29 21:52:25 +0600

    learning python

---
+++ 1.data_types.py
@@ -0,0 +1,22 @@
+
+# python e 6 doroner data type ache
+
+# Numeric Type => Int, Float, Complex
+# Sequence Type => String, List, Tuple
+# Mapping => Dictionary
+# Set => Set, Frozenset
+# Boolean => bool
+# None => NoneType
+
+
+name = "Kawsar"
+print(type(name))
+
+
+
+
+
+
+
+
+
