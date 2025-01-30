1172ff05dc610869565bb4e0b98a1c75da1fece8
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-01-30 21:30:06 +0600

    practiced list method

---
+++ 6.list_method.py
@@ -0,0 +1,58 @@
+
+friends = ['Kawsar', 'Ahmed', 'Samim', 'Javed']
+
+# add a item in end of list
+# friends.append('Mohammad')
+# print(friends)
+
+
+# clear list
+# friends.clear()
+# print(friends)
+
+
+# copy list
+# copied_friends = friends.copy()
+# print(copied_friends)
+
+
+# get a count which exact match
+# num = friends.count('Samim')
+# print(num)
+
+
+# extend main list
+# more_friends = ['Mijan', 'Nahid', 'Ajel']
+# friends.extend(more_friends)
+# print(friends)
+
+
+# return first element index
+# index_num = friends.index('Kawsar')
+# print(index_num)
+
+
+# insert in a position of list
+# friends.insert(2, 'King')
+# print(friends)
+
+
+# remove specific element by index
+# friends.pop(2)
+# print(friends)
+
+
+
+# reverse list
+# friends.reverse()
+# print(friends)
+
+# numbers = [1, 3, 5, 7, 9, 10]
+# numbers.reverse()
+# print(numbers)
+
+
+
+# sorting a list
+friends.sort()
+print(friends)
