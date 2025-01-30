bb601ba5a0b91c9f02fd45128058a6710c2ed08b
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-01-30 21:57:06 +0600

    practiced python dictionary

---
+++ 7.dictionary.py
@@ -0,0 +1,48 @@
+
+car = {"brand": "Toyota", "model": "HF", "price": 520000}
+# print(car)
+
+
+# access specific value
+# print(car['model'])
+
+
+# get keys
+# print(car.keys())
+
+
+# get values
+# print(car.values())
+
+
+#  get key values
+# print(car.items())
+
+
+# iterate
+# for key in car:
+#     # print(key)
+#     print(car[key])
+
+
+# if i get key values 
+# for key, value in car.items():
+#     print(key, value)
+
+
+
+# dictionary method
+# clear()
+# copy()
+# fromKeys()
+# get()
+# items()
+# keys()
+# pop()
+# popItem()
+# setDefault()
+# update()
+# values()
+
+
+
