217c721ba8d1d2a9c51a2600b285f3070eecad36
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-01 22:19:13 +0600

    practiced function

---
+++ 8.function.py
@@ -0,0 +1,21 @@
+
+# Function
+# Built in function
+# User defined function
+
+def sum(x, y):
+    return x + y
+
+result = sum(100, 200)
+print(result)
+
+
+# global variable kivabe declare korte hoy inside the function
+# lamda function kivabe kaj kore
+# string methods
+
+
+
+
+
+
