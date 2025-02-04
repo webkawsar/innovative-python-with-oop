24d59b3a8ca8f3e49610ecf4bbe34a90c6db96af
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-02-04 23:53:01 +0600

    practiced oop basic initialization, instance, attribute, method

--- 9.class_basic.py
+++ 9.class_basic.py
@@ -1,5 +1,35 @@
-class Student:
-    name = 'Kawsar Ahmed'
+# class Student:
+#     name = 'Kawsar Ahmed'
 
-student = Student()
-print(student.name)
+# student = Student()
+# print(student.name)
+
+
+class Student:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def info(self):
+        print(self)
+        return self.id, self.name
+
+
+id_no = 101
+name = "Kawsar"
+student1 = Student(id_no, name)
+st_id, st_name = student1.info()
+print(st_id, 'st_id')
+print(st_name, 'st_name')
+
+
+
+
+
+
+
+
+
+
+
+
